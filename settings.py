ad94d556a827880c6f91227e5d98420b1156f79a
Rajdeep Bharati <rajdeep@macports.org>
2020-02-03 19:07:08 +0530

    Fix mongodb settings

--- settings.py
+++ settings.py
@@ -1,5 +1,16 @@
-MONGO_URL = 'mongodb'
-MONGO_PORT = str(27017)
-MONGO_URI = 'mongodb://' + MONGO_URL + ':' + MONGO_PORT + '/'
-DEBUG = True
-SECRET_KEY = 'secret'
+import os
+
+
+if (os.environ.get('FLASK_ENV') == 'development'
+        or os.environ.get('FLASK_ENV') == 'dev'):
+    MONGO_URL = 'localhost'
+    MONGO_PORT = str(27017)
+    MONGO_URI = 'mongodb://' + MONGO_URL + ':' + MONGO_PORT + '/'
+    DEBUG = True
+    SECRET_KEY = 'secret'
+else:
+    MONGO_URL = 'mongodb'
+    MONGO_PORT = str(27017)
+    MONGO_URI = 'mongodb://' + MONGO_URL + ':' + MONGO_PORT + '/'
+    DEBUG = True
+    SECRET_KEY = 'secret'
