b12bb4dac914b03c8d38b4b572e47e8f5d8560cb
Rajdeep Bharati <rajdeep@macports.org>
2020-02-03 15:21:04 +0530

    Dockerize the project

--- settings.py
+++ settings.py
@@ -1,15 +1,5 @@
-import os
-
-
-if os.environ.get('FLASK_ENV') == 'development':
-    MONGO_URL = 'mongodb'
-    MONGO_PORT = str(27017)
-    MONGO_URI = 'mongodb://' + MONGO_URL + ':' + MONGO_PORT + '/'
-    DEBUG = True
-    SECRET_KEY = 'secret'
-else:
-    MONGO_URL = 'localhost'
-    MONGO_PORT = str(27017)
-    MONGO_URI = 'mongodb://' + MONGO_URL + ':' + MONGO_PORT + '/'
-    DEBUG = True
-    SECRET_KEY = 'secret'
+MONGO_URL = 'mongodb'
+MONGO_PORT = str(27017)
+MONGO_URI = 'mongodb://' + MONGO_URL + ':' + MONGO_PORT + '/'
+DEBUG = True
+SECRET_KEY = 'secret'
